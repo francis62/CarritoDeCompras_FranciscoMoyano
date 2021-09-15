9e782b32b5e3aa8d75056c6da2081f48eb2406d8
Francisco Moyano <franciscomoyano424@gmail.com>
2021-09-15 19:18:29 -0300

    Desafio - Carrito de compras

---
+++ CarritoDeCompras/CarritoDeCompras/Carrito.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarritoDeCompras
+{
+    class Carrito
+    {
+        private bool primeraVez = true;
+        private static int CantidadDeCamisasAñadidas;
+        private int CantidadDeCamisasEliminadas;
+        private double DescuentoAplicado = 0.0;
+        private int PrecioUnitarioCamisa = 1000;
+        private double PrecioTotalSinDescuento;
+        private double PrecioTotalConDescuento;
+        
+        static public int GetCantidadDeCamisasAñadidas
+        {
+            get
+            {
+                return CantidadDeCamisasAñadidas;
+            }
+        }
+
+        public void AñadirCamisas()
+        {
+            if (primeraVez == true)
+            {
+                Console.Write("Cuantas camisas desea añadir: ");
+                CantidadDeCamisasAñadidas = int.Parse(Console.ReadLine());
+
+                primeraVez = false; 
+            }
+            else
+            {
+                int auxiliarCantidad;
+
+                Console.Write("Cuantas camisas desea añadir: ");
+                auxiliarCantidad = int.Parse(Console.ReadLine());
+
+                CantidadDeCamisasAñadidas = CantidadDeCamisasAñadidas + auxiliarCantidad;
+            }
+        }
+
+        public void EliminarCamisas()
+        {
+            if(CantidadDeCamisasAñadidas == 0)
+            {
+                Console.WriteLine("El carrito se encuentra vacio en estos momentos");
+                Console.WriteLine("Debe añadir camisas antes de eliminarlas...");
+            }
+            else
+            {
+                Console.Write("Cuantas camisas desea eliminar:");
+                CantidadDeCamisasEliminadas = int.Parse(Console.ReadLine());
+
+                if (CantidadDeCamisasEliminadas > CantidadDeCamisasAñadidas)
+                {
+                    Console.WriteLine("No se pueden eliminar mas camisas de las que hay");
+                }
+                else
+                {
+                    CantidadDeCamisasAñadidas = CantidadDeCamisasAñadidas - CantidadDeCamisasEliminadas;
+
+                    if (CantidadDeCamisasAñadidas > 5)
+                    {
+                        DescuentoAplicado = 0.2;
+                    }
+                    else if (CantidadDeCamisasAñadidas >= 3 || CantidadDeCamisasAñadidas <= 5)
+                    {
+                        DescuentoAplicado = 0.1;
+
+                    }else if(CantidadDeCamisasAñadidas < 3)
+                    {
+                        DescuentoAplicado = 0.0;
+                    }
+                }   
+            }
+        }
+
+        public void CalcularPrecioSinDescuento()
+        {
+            PrecioTotalSinDescuento  = PrecioUnitarioCamisa * CantidadDeCamisasAñadidas;
+        }
+
+        public void CalcularPrecioConDescuento()
+        {
+            if(CantidadDeCamisasAñadidas > 5)
+            {
+                DescuentoAplicado = 0.20;
+
+                PrecioTotalConDescuento = PrecioTotalSinDescuento - (PrecioTotalSinDescuento * DescuentoAplicado);
+
+            }else if(CantidadDeCamisasAñadidas >= 3 || CantidadDeCamisasAñadidas <= 5)
+            {
+                DescuentoAplicado = 0.10;
+
+                PrecioTotalConDescuento = PrecioTotalSinDescuento - (PrecioTotalSinDescuento * DescuentoAplicado);
+            }
+            else
+            {
+                PrecioTotalConDescuento = PrecioTotalSinDescuento;
+            }
+        }
+        public double GetPrecioTotalSinDescuento
+        {
+            get
+            {
+                return PrecioTotalSinDescuento;
+            }
+        }
+
+        public double GetPrecioTotalConDescuento
+        {
+            get
+            {
+                return PrecioTotalConDescuento;
+            }
+        }
+
+        public double RetornarDescuento()
+        {
+            double auxiliar;
+
+            auxiliar = (DescuentoAplicado * 100);
+
+            return auxiliar;
+        }
+    }
+}
---
+++ CarritoDeCompras/CarritoDeCompras/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarritoDeCompras
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Carrito miCarroDeCompra = new Carrito();
+
+            int opcionElegida = 0;
+            String opcionSalir;
+            bool salir = false;
+
+            do
+            {
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine("SHOPPING ONLINE DE CAMISAS - Venta miinorista y mayorista");
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine("MENÚ PRINCIPAL");
+                Console.WriteLine("1 -> Añadir camisa al carro de compras");
+                Console.WriteLine("2 -> Eliminar camisa al carro de compras");
+                Console.WriteLine("3 -> Salir");
+                Console.WriteLine("---------------------------------------------------------");
+                Console.WriteLine("             -  Cantidad de camisas en el carro de compras: "+Carrito.GetCantidadDeCamisasAñadidas+" unidad/es");
+                Console.WriteLine("             -  Precio unitario: $1000");
+                Console.WriteLine("             -  Precio total sin descuento: $"+miCarroDeCompra.GetPrecioTotalSinDescuento);
+                Console.WriteLine("             -  Tipo de descuento aplicado: %"+miCarroDeCompra.RetornarDescuento());
+                Console.WriteLine("             -  Precio final con descuento: $"+miCarroDeCompra.GetPrecioTotalConDescuento);
+                Console.WriteLine("---------------------------------------------------------");
+                Console.Write("Ingrese una opción del menú:");
+                opcionElegida = int.Parse(Console.ReadLine());
+
+                switch (opcionElegida)
+                {
+                    case 1:
+                        miCarroDeCompra.AñadirCamisas();
+                        miCarroDeCompra.CalcularPrecioSinDescuento();
+                        miCarroDeCompra.CalcularPrecioConDescuento();
+                        break;
+
+                    case 2:
+                        miCarroDeCompra.EliminarCamisas();
+                        miCarroDeCompra.CalcularPrecioSinDescuento();
+                        miCarroDeCompra.CalcularPrecioConDescuento();
+                        break;
+
+                    case 3:
+                        Console.Clear();
+                        Console.WriteLine("¿Está seguro que desea salir? S/N");
+                        opcionSalir = Console.ReadLine();
+
+                        if (opcionSalir == "S" || opcionSalir == "s")
+                        {
+                            Console.WriteLine("-------------------------------");
+                            Console.WriteLine("Saliendo del shopping online...");
+                            Console.WriteLine("-------------------------------");
+                            salir = true;
+
+                        }else if(opcionSalir == "N" || opcionSalir == "n")
+                        {
+                            Console.WriteLine("");
+                        } 
+                        break;
+
+                    default:
+                        Console.WriteLine("------------------------------------");
+                        Console.WriteLine("Opcin ingresada de manera incorrecta");
+                        Console.WriteLine("Ingreselo nuevamente...");
+                        Console.WriteLine("------------------------------------");
+                        break;
+                }
+
+            } while (salir == false);
+            
+        }
+    }
+}
